3286a2374a000bd2884f29db8dcfb367d11f2641
SinthuPriya <sinthuj.engg@gmail.com>
2022-09-29 16:15:25 +0530

    code by sinthu

---
+++ src/javaproject/List.java
@@ -0,0 +1,81 @@
+package javaproject;
+
+import java.util.ArrayList;
+
+public class List {
+
+	public static void main(String[] args) {
+		ArrayList li=new ArrayList();
+		li.add(25);
+		li.add("Sinthu");
+		li .add (123.56f);
+		li.add(25);
+		System.out.println(li);
+		
+		//for loop
+		for(int i=0; i<li.size();i++) {
+			System.out.println(li.get(i));
+		
+	
+	}
+
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+	
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+		
+	
+
+	
+
+	
+		
+	
+
+
